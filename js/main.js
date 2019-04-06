d6d31d4602ed57495dcdf51cc1cdd86819a2d5b5
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2019-04-07 07:09:06 +1000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,28 @@
-$('.marquee').marquee({
-    //speed in milliseconds of the marquee
-    duration: 15000,
-    //gap in pixels between the tickers
-    gap: 10,
-    //time in milliseconds before the marquee will start animating
-    delayBeforeStart: 0,
-    //'left' or 'right'
-    direction: 'left',
-    //true or false - should the marquee be duplicated to show an effect of continues flow
-    duplicated: true
-});
+// When the user clicks on <span> (x), close the modal
+function rabbit() {
+      var title = document.querySelector("#learn-more");
+      title.style.display = "block";
+      var joe = document.querySelector("#work-inner");
+      joe.style.width = "calc(50% - 52.5px)";
+    }
+
+
+// When the user clicks on <span> (x), close the modal
+function duck() {
+      var title = document.querySelector("#learn-more");
+      title.style.display = "none";
+      var joe = document.querySelector("#work-inner");
+      joe.style.width = "";
+    }
+
+// When the user clicks on <span> (x), close the modal
+function goose() {
+      var something = document.querySelector("#project-container");
+      something.style.display = "flex";
+      var bingo = document.querySelector("#work-inner");
+      bingo.style.display = "none";
+     var trevor = document.querySelector("#learn-more");
+      trevor.style.display = "none";
+       var trevor = document.querySelector(".work");
+      trevor.style.overflow = "hidden";
+    }
