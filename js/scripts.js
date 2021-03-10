43808cb4898d73174cf04476a54ee1b919edf7ad
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2021-03-10 16:31:22 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ function openProjects() {
 
 function openFA() {
     document.body.style.gridTemplateColumns = "25vw 50vw 25vw";
-    document.getElementByClassName("popup").style.left = "12.5vw";
-    document.getElementByClassName("popup").style.width = "25vw";
-    document.getElementByClassName("video").style.width = "250px";
+    document.getElementsByClassName("popup").style.left = "12.5vw";
+    document.getElementsByClassName("popup").style.width = "25vw";
+    document.getElementsByClassName("video").style.width = "250px";
 }
