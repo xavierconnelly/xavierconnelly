6ff1abdd4bd92b7cd629c1330b4bf237b929cc9a
Xavier Connelly <44646743+xavierconnelly@users.noreply.github.com>
2021-03-10 14:28:28 +1100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,7 @@ function openProjects() {
     document.getElementById("clickme-center").style.display = "none";
     document.getElementById("titles").style.display = "block";
 }
+
+function openFA() {
+    document.body("left").style.grid-template-columns = "25vw 50vw 25vw";
+}
